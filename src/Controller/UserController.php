1f04c2eed0c879031701b062c6d1923105b48309
jamaisseule <nganttqgcd201774@fpt.edu.vn>
2022-11-03 14:04:42 +0700

    edit user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,10 +5,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use ContainerORvOApe\getErrorHandler_ErrorRenderer_HtmlService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use function MongoDB\BSON\toJSON;
 
 /**
  * @Route("/user")
@@ -49,11 +52,17 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}", name="app_user_show", methods={"GET"})
      */
-    public function show(User $user): Response
+    public function show(Request $request, User $user): Response
     {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
+        /** @var \App\Entity\User $currentUser */
+        $currentUser = $this->getUser();
+        if($currentUser->getId() == $user->getId()) {
+            return $this->render('user/show.html.twig', [
+                'user' => $user,
+            ]);
+        } else {
+            return $this->redirectToRoute('app_product_index');
+        }
     }
 
     /**
@@ -61,20 +70,27 @@ class UserController extends AbstractController
      */
     public function edit(Request $request, User $user, UserRepository $userRepository): Response
     {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->add($user, true);
-
-            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        /** @var \App\Entity\User $currentUser */
+        $currentUser = $this->getUser();
+        if($currentUser->getId() == $user->getId()){
+            $form = $this->createForm(UserType::class, $user);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $userRepository->add($user, true);
+                return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+            }
+            return $this->renderForm('user/edit.html.twig', [
+                'user' => $user,
+                'form' => $form,
+            ]);
+        } else {
+            return $this->redirectToRoute('app_product_index');
         }
 
-        return $this->renderForm('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
+        }
+
+
+
 
     /**
      * @Route("/{id}", name="app_user_delete", methods={"POST"})
